eee874f7294799ee01e2ce81baf1a341511931e7
kunalbisht123 <kunal.bisht@shiprocket.com>
2022-09-10 17:13:12 +0530

    Added update and users API

--- config/routes.js
+++ config/routes.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
 const policyPath = '../api/policies';
 
+console.log('Setting up policies');
 const appliedPolicy = {
     '/add' : ['/user-authorization'],
     '/fetch' : ['/user-authorization'],
     '/logout' : ['/user-authorization'],
+    '/update' : ['/user-authorization']
 };
 
 for(const path in appliedPolicy){
+    console.log(path)
+    console.log(appliedPolicy[path]);
     appliedPolicy[path].forEach((item)=>{
         router.use(path, require(policyPath + item).fn);
     });
@@ -22,9 +26,11 @@ const controllers = [
     '/health-check GET /health-check',
     '/login POST /auth/login',
     '/register POST /auth/register',
-    '/add POST /exercise/add',
+    '/add PUT /exercise/add',
     '/fetch GET /exercise/fetch',
-    '/logout GET /logout'
+    '/logout GET /logout',
+    '/users GET /users',
+    '/update PUT /exercise/update'
 ];
 
 console.log('Setting up routes');
